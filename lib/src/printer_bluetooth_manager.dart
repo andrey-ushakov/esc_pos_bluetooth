278b7b51c8bdd911cbc71d4f34bc38ac75f7272d
louieseno <senolouiejohn@gmail.com>
2020-11-20 13:45:37 +0800

    test: remove plus 2 duration on scan

--- lib/src/printer_bluetooth_manager.dart
+++ lib/src/printer_bluetooth_manager.dart
@@ -110,7 +110,7 @@ class PrinterBluetoothManager {
     }
 
     // We have to rescan before connecting, otherwise we can connect only once
-    await _bluetoothManager.startScan(timeout: Duration(seconds: _timeOut + 2));
+    await _bluetoothManager.startScan(timeout: Duration(seconds: _timeOut));
     await _bluetoothManager.stopScan();
 
     // Connect
