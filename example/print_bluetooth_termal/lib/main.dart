9f90e164bddc70c460b25f653e84ad9d023f7fa7
Reza Indrawan <me.rezaindrawan@gmail.com>
2021-01-16 11:45:56 +0700

    Fix 1.2.30

--- example/print_bluetooth_termal/lib/main.dart
+++ example/print_bluetooth_termal/lib/main.dart
@@ -71,45 +71,130 @@ class _MyHomePageState extends State<MyHomePage> {
     final generator = Generator(PaperSize.mm80, profile);
     List<int> ticket = [];
 
+
     // Print image
     final ByteData data = await rootBundle.load('assets/rabbit_black.jpg');
     final Uint8List bytes = data.buffer.asUint8List();
     final Image image = decodeImage(bytes);
     // ticket.image(image);
-    ticket += generator.text(" "); //=============================> !!!! IMPORTANT
 
-    ticket += generator.text("TESTING PRINT",
-      styles: PosStyles(
-        align: PosAlign.center,
-        bold: true
-      )
-    );
+    ticket += generator.text(" "); //===========> IMPORTAN!!!
 
-    ticket += generator.hr();
-
-    ticket += generator.text("Items : ",
+    ticket += generator.text('GROCERYLY',
         styles: PosStyles(
-            align: PosAlign.left,
-            bold: true
-        )
-    );
+          align: PosAlign.center,
+          height: PosTextSize.size2,
+          width: PosTextSize.size2,
+        ),
+        linesAfter: 1);
 
-    ticket += generator.text("Latop 1 ",
-        styles: PosStyles(
+    ticket += generator.text('889  Watson Lane', styles: PosStyles(align: PosAlign.center));
+    ticket += generator.text('New Braunfels, TX', styles: PosStyles(align: PosAlign.center));
+    ticket += generator.text('Tel: 830-221-1234', styles: PosStyles(align: PosAlign.center));
+    ticket += generator.text('Web: www.example.com',
+        styles: PosStyles(align: PosAlign.center), linesAfter: 1);
+
+    ticket += generator.hr(ch: '-', linesAfter: 1);
+
+    ticket += generator.row([
+      PosColumn(text: 'Qty ', width: 1),
+      PosColumn(text: 'Item ', width: 7),
+      PosColumn(
+          text: 'Price ', width: 2, styles: PosStyles(align: PosAlign.right)),
+      PosColumn(
+          text: 'Total ', width: 2, styles: PosStyles(align: PosAlign.right)),
+    ]);
+
+    ticket += generator.row([
+      PosColumn(text: '2', width: 1),
+      PosColumn(text: 'ONION RINGS ', width: 7),
+      PosColumn(
+          text: '0.99 ', width: 2, styles: PosStyles(align: PosAlign.right)),
+      PosColumn(
+          text: '1.98 ', width: 2, styles: PosStyles(align: PosAlign.right)),
+    ]);
+    ticket += generator.row([
+      PosColumn(text: '1', width: 1),
+      PosColumn(text: 'PIZZA ', width: 7),
+      PosColumn(
+          text: '3.45 ', width: 2, styles: PosStyles(align: PosAlign.right)),
+      PosColumn(
+          text: '3.45 ', width: 2, styles: PosStyles(align: PosAlign.right)),
+    ]);
+    ticket += generator.row([
+      PosColumn(text: '1', width: 1),
+      PosColumn(text: 'SPRING ROLLS ', width: 7),
+      PosColumn(
+          text: '2.99 ', width: 2, styles: PosStyles(align: PosAlign.right)),
+      PosColumn(
+          text: '2.99 ', width: 2, styles: PosStyles(align: PosAlign.right)),
+    ]);
+    ticket += generator.row([
+      PosColumn(text: '3', width: 1),
+      PosColumn(text: 'CRUNCHY STICKS ', width: 7),
+      PosColumn(
+          text: '0.85 ', width: 2, styles: PosStyles(align: PosAlign.right)),
+      PosColumn(
+          text: '2.55 ', width: 2, styles: PosStyles(align: PosAlign.right)),
+    ]);
+
+    ticket += generator.hr(ch: '-', linesAfter: 1);
+
+    ticket += generator.row([
+      PosColumn(
+          text: 'TOTAL ',
+          width: 6,
+          styles: PosStyles(
+            height: PosTextSize.size2,
+            width: PosTextSize.size2,
+          )),
+      PosColumn(
+          text: '\$10.97',
+          width: 6,
+          styles: PosStyles(
             align: PosAlign.right,
-            bold: true
-        )
-    );
-    ticket += generator.text("Latop 2 ",
-        styles: PosStyles(
-            align: PosAlign.right,
-            bold: true
-        )
-    );
+            height: PosTextSize.size2,
+            width: PosTextSize.size2,
+          )),
+    ]);
 
+    ticket += generator.hr(ch: '=', linesAfter: 1);
 
+    ticket += generator.row([
+      PosColumn(
+          text: 'Cash',
+          width: 7,
+          styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+      PosColumn(
+          text: '\$15.00',
+          width: 5,
+          styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+    ]);
+    ticket += generator.row([
+      PosColumn(
+          text: 'Change',
+          width: 7,
+          styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+      PosColumn(
+          text: '\$4.03',
+          width: 5,
+          styles: PosStyles(align: PosAlign.right, width: PosTextSize.size2)),
+    ]);
+
+    ticket += generator.feed(2);
+    ticket += generator.text('Thank you!',
+        styles: PosStyles(align: PosAlign.center, bold: true));
+
+    final now = DateTime.now();
+    final formatter = DateFormat('MM/dd/yyyy H:m');
+    final String timestamp = formatter.format(now);
+    ticket += generator.text(timestamp,
+        styles: PosStyles(align: PosAlign.center), linesAfter: 2);
+
+    ticket += generator.feed(2);
     ticket += generator.cut();
     return ticket;
+
   }
 
   void _testPrint(PrinterBluetooth printer) async {
